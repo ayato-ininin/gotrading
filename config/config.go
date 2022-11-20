c1efa62f6072ec72844bc2924f70a804787f74b6
ayato yamauchi <>
2022-11-20 11:15:32 +0900

    add bitflyerアプリ作成、config設定
    config.ini追加必要あり。
    go get gopkg.in/ini.v1
    設定用に上記追加

---
+++ config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import(
+	"gopkg.in/ini.v1"
+	"log"
+	"os"
+)
+
+type ConfigList struct{
+	ApiKey string
+	ApiSecret string
+}
+
+var Config ConfigList//グローバル変数
+
+//パッケージを読み込むときに、一回だけ読み込まれる。
+//main.goからimportされたとき、設定ファイルを読み込むことができる。
+//それをグローバル変数に入れてるから、main.goからグローバル変数として呼び出せる仕組み。
+//別途、config.iniファイルの作成が必要。
+func init()  {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Printf("Failed to read file: $v", err)
+		os.Exit(1)//configファイルが読めなかったら出る。
+	}
+
+	Config = ConfigList{
+		ApiKey: cfg.Section("bitflyer").Key("api_key").String(),
+		ApiSecret: cfg.Section("bitflyer").Key("api_secret").String(),
+	}
+}
