52a93e598ec27ea3bc836abdc0a11b3ef7b42098
ayato yamauchi <>
2022-11-20 13:08:45 +0900

    add logging追加(utility:役に立つもの、パッケージも追加)
    config.iniに出力するファイル名追加
    configからそのファイルを呼び出して設定

--- config/config.go
+++ config/config.go
@@ -9,6 +9,7 @@ import(
 type ConfigList struct{
 	ApiKey string
 	ApiSecret string
+	LogFile string
 }
 
 var Config ConfigList//グローバル変数
@@ -27,5 +28,6 @@ func init()  {
 	Config = ConfigList{
 		ApiKey: cfg.Section("bitflyer").Key("api_key").String(),
 		ApiSecret: cfg.Section("bitflyer").Key("api_secret").String(),
+		LogFile: cfg.Section("gotrading").Key("log_file").String(),
 	}
 }
--- main.go
+++ main.go
@@ -3,9 +3,24 @@ package main
 import (
 	"fmt"
 	"gotrading/config"
+	"gotrading/utils"
+	"log"
 )
 
+/*
+vscodeでimportとか定義移動の補完付ける場合、setting.jsonの修正がいる。
+また、vscodeからgoplsのインストールのおすすめくるけど、おそらくそこから押しても
+go getになるから、go1.16以上とかだと使えないぽい。
+go install golang.org/x/tools/gopls@latest
+
+https://qiita.com/sasaron397/items/ec285b64607c1e7662e0
+"go.gopath": "/Users/ayatoymauchi/go",
+"gopls": { "experimentalWorkspaceModule": true}
+*/
+
 func main()  {
+	utils.LoggingSettings(config.Config.LogFile)
+	log.Println("test")
 	fmt.Println(config.Config.ApiKey)
 	fmt.Println(config.Config.ApiSecret)
 }
